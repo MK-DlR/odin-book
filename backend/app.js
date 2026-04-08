71e013127dec2a2d5aeea7a87bec7fc143d0a035
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-04-08 15:41:42 -0400

    build: install dependencies
    delete unnecessary files and styling
    set up prisma schema (in progress)

---
+++ backend/app.js
@@ -0,0 +1,71 @@
+// backend/app.js
+
+// require("dotenv").config();
+
+// imports
+const express = require("express");
+const path = require("path");
+// const { prisma } = require("./lib/prisma");
+const cors = require("cors");
+
+// initialize app
+const app = express();
+
+// middleware
+app.use(express.json()); // parse json for apis
+app.use(express.urlencoded({ extended: true })); // parse form data
+app.use(express.static(path.join(__dirname, "public"))); // static files
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  }),
+);
+
+// routes
+// home route (before routers)
+app.get("/", (req, res) => {
+  res.json({ message: "API running" });
+});
+
+// router setup
+/*
+const crouterNameRouter = require("./routes/routerName.js");
+app.use("/routerName", crouterNameRouter);
+*/
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ error: "Not found." });
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+// start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async (error) => {
+  if (error) throw error;
+  console.log(`Server running on port ${PORT}.`);
+
+  // test database connection
+  /*
+  try {
+    await prisma.$connect();
+    console.log("Database connected");
+  } catch (err) {
+    console.error("Database connection failed:", err);
+  }
+    */
+});
+
+// graceful shutdown
+/*
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  console.log("Database disconnected.");
+  process.exit(0);
+});
+*/
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// frontend/src/App.jsx
+
+function App() {
+
+  return (
+    <>
+      <div>
+        hi
+      </div>
+    </>
+  )
+}
+
+export default App
