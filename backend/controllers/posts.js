80b98e62f0e717f8b724a30d7cc79970933f11ee
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-04-14 11:51:16 -0400

    chore: set up posts router and controller

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -3,43 +3,57 @@
 // imports
 const { prisma } = require("../lib/prisma.js");
 
-// posts (+ replies)
-// TODO:
-// create post
-// adds post to database
-// adds post to list of user's posts
-// displays post on followers dashboards
-// and user's profile dashboard
+// posts/replies
+// TODO: createPost
+const createPost = async (req, res, next) => {
+  try {
+    // adds post to database
+    // adds post to list of user's posts
+    // note about replies:
+    // can see list of who replied to the post
+    // and the reply itself
+  } catch (err) {
+    return next(err);
+  }
+};
 
-// TODO:
-// delete post
-// delete post from database
-// removes post from list of user's posts
-// removes post from followers dashboards
-// and user's profile dashboard
+// TODO: deletePost
+const deletePost = async (req, res, next) => {
+  try {
+    // delete post from database
+    // removes post from list of user's posts
+    // deletes any reposts/likes
+    // orphans any replies
+  } catch (err) {
+    return next(err);
+  }
+};
 
 // reposts
-// TODO:
-// create repost
-// adds repost to list of user's posts
-// displays repost on followers dashboards
-// and user's profile dashboard
-
-// TODO:
-// remove repost
-// removes repost from list of user's posts
-// removes repost from followers dashboards
-// and user's profile dashboard
+// TODO: manageRepost
+const manageRepost = async (req, res, next) => {
+  try {
+    // adds repost to list of user's posts
+    // removes repost from list of user's posts
+  } catch (err) {
+    return next(err);
+  }
+};
 
 // likes
-// TODO:
-// create like
-// adds to user's likes list
-
-// TODO:
-// remove like
-// removes from user's likes list
+// TODO: manageLike
+const manageLike = async (req, res, next) => {
+  try {
+    // adds post to user's likes list
+    // removes post from user's likes list
+  } catch (err) {
+    return next(err);
+  }
+};
 
 module.exports = {
-  // function names
+  // createPost,
+  // deletePost,
+  // manageRepost,
+  // manageLike,
 };
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,10 +1,21 @@
 // backend/routes/posts.js
 
+// imports
 const express = require("express");
 const router = express.Router();
+
 const postsController = require("../controllers/posts.js");
+
 const { authJWT } = require("../middleware/auth.js");
 
-// routes
+// post routes
+// router.post("/new-post", authJWT, postsController.createPost);
+// router.delete("/delete/:id", authJWT, postsController.deletePost);
+
+// repost routes
+// router.post("/repost/:id", authJWT, postsController.manageRepost);
+
+// like routes
+// router.post("/like/:id", authJWT, postsController.manageLike);
 
 module.exports = router;
