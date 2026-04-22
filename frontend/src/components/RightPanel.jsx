8c08838b7976114d5e7393061763d501d5cec506
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-04-22 13:30:56 -0400

    style: fix spacing and update comments
    remove unnecessary comments from RightPanel
    add comments to text.css

--- frontend/src/components/RightPanel.jsx
+++ frontend/src/components/RightPanel.jsx
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function RightPanel() {
-
     // search input
     const searchBar = 
         <div id="search-container">
@@ -16,9 +15,6 @@ function RightPanel() {
             />
             <FontAwesomeIcon icon={faSearch} className="search-icon" />
         </div>
-
-    // TODO: active vs inactive search 
-    // should have different colored icons
 
     return (
         <div className="right-panel">
