7cc39295859faab6b8fe772863e21a5eedde0918
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-04-22 13:28:06 -0400

    style: update placeholder theme colors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import "./styles/theme.css";
-import './styles/index.css'
+import "./styles/theme.css"
+import './styles/base.css'
 import './styles/search.css'
+import './styles/text.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
