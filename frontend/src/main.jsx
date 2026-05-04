4894b0d4cdf59842504ce21fe7a1a5d06f3df75b
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-05-04 12:18:38 -0400

    style: reorganize and consolidate css files
    separate buttons and icons css sheets
    combine search and suggested css sheets
    consolidate LeftPanel and RightPanel css folders into individual folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,14 +7,14 @@ import App from './App.jsx'
 
 import "./styles/shared/theme.css"
 import './styles/shared/base.css'
-import './styles/shared/buttons-icons.css'
+import './styles/shared/buttons.css'
+import './styles/shared/icons.css'
 import './styles/shared/layout.css'
 import './styles/shared/text.css'
 
 import './styles/CenterPanel/profile.css'
-import './styles/LeftPanel/tabs.css'
-import './styles/RightPanel/search.css'
-import './styles/RightPanel/suggested.css'
+import './styles/individual/right-panel.css'
+import './styles/individual/tabs.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
