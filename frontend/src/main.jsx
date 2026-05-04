1b1ad464c8f6fe67528726f4caf5edc939e76919
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-05-04 12:49:59 -0400

    chore: rename CenterPanel css folder to center

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import "./styles/shared/theme.css"
 import './styles/shared/base.css'
 import './styles/shared/buttons.css'
 import './styles/shared/icons.css'
 import './styles/shared/layout.css'
 import './styles/shared/text.css'
+import "./styles/shared/theme.css"
 
-import './styles/CenterPanel/profile.css'
+import './styles/center/profile.css'
 import './styles/individual/right-panel.css'
 import './styles/individual/tabs.css'
 
