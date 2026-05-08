83dd37f67c84bdd1f2890f320ed19e9ac0f0606d
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-05-08 12:16:21 -0400

    chore: create .env.example files

--- frontend/src/components/CenterPanel/FullsizeImageViewer.jsx
+++ frontend/src/components/CenterPanel/FullsizeImageViewer.jsx
@@ -1,5 +1,13 @@
 // frontend/src/components/FullsizeImageViewer.jsx
 
+// imports
+import { useEffect } from "react";
+import { useFullsizeImage } from "../../contexts/FullsizeImageContent";
+
+function FullsizeImageViewer() {
+    
+}
+
 // modal that displays image
 // reads from context
 // renders when there's an image
